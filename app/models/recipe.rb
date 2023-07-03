3d78e629e45c34928c44de4ec11fbcaa1517b061
matistaken <mathieu@lamanufacture.dev>
2023-07-03 23:10:44 +0200

    feat: allowing `OR` style ingredient search

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,10 +3,14 @@
 class Recipe < ApplicationRecord
   include PgSearch::Model
 
-  pg_search_scope :by_ingredients, against: :ingredients, using: {
-    tsearch: {
-      dictionary: "english",
-      tsvector_column: "search_ingredients"
-    }
-  }
+  pg_search_scope :by_ingredients,
+                  against: :ingredients,
+                  order_within_rank: "recipes.rating DESC",
+                  using: {
+                    tsearch: {
+                      dictionary: "english",
+                      tsvector_column: "search_ingredients",
+                      any_word: true
+                    }
+                  }
 end
