f60e6eaa4e17435857818e7a70fbe49bec66c9db
matistaken <mathieu@lamanufacture.dev>
2023-07-09 13:05:55 +0200

    fix: ingredients picker is now a regular form

--- app/javascript/components/IngredientsPicker.tsx
+++ app/javascript/components/IngredientsPicker.tsx
@@ -3,48 +3,56 @@ import { Button, Form, Tag } from 'react-bulma-components'
 
 export interface IngredientsPickerProps {
   onChange: Function
+  ingredients: string[] | null
 }
 
-function IngredientsPicker({ onChange }: IngredientsPickerProps) {
+function IngredientsPicker({ onChange, ingredients }: IngredientsPickerProps) {
   const [ingredientToAdd, setIngredientToAdd] = useState('')
-  const [ingredients, setIngredients] = useState<string[]>([])
+  const [ingredientsState, setIngredientsState] = useState<string[]>(ingredients || [])
   const notInitialRender = useRef(false)
 
-  const onAddIngredient = () => {
-    if (ingredientToAdd == '') { return }
+  const onAddIngredient = (e) => {
+    e.preventDefault()
+    notInitialRender.current = true
 
-    setIngredients(ingredients.concat(ingredientToAdd))
+    setIngredientsState(ingredientsState.concat(ingredientToAdd))
     setIngredientToAdd('')
   }
   const onRemoveIngredient = (ingredient: string) => {
-    setIngredients(ingredients.filter(i => i !== ingredient))
+    notInitialRender.current = true
+
+    setIngredientsState(ingredientsState.filter(i => i !== ingredient))
   }
 
   useEffect(() => {
-    notInitialRender.current ? onChange(ingredients) : notInitialRender.current = true
-  }, [ingredients])
+    if (notInitialRender.current) { onChange(ingredientsState) }
+  }, [ingredientsState])
 
   return (
     <>
-      <Form.Field className="is-justify-content-center" kind="addons">
-        <Form.Control>
-          <Form.Input
-            value={ingredientToAdd}
-            onChange={(e) => setIngredientToAdd(e.target.value)}
-            placeholder="e.g. Cheese" type="text" />
-        </Form.Control>
-        <Form.Control>
-          <Button
-            onClick={onAddIngredient}
-            color="success"
-          >
-            Add ingredient
-          </Button>
-        </Form.Control>
-      </Form.Field>
+      <form className="mb-3" onSubmit={onAddIngredient}>
+        <Form.Field className="is-justify-content-center" kind="addons">
+          <Form.Control>
+            <Form.Input
+              value={ingredientToAdd}
+              onChange={(e) => setIngredientToAdd(e.target.value)}
+              placeholder="e.g. Cheese"
+              type="text"
+              required />
+          </Form.Control>
+          <Form.Control>
+            <Button
+              type="submit"
+              color="success"
+            >
+              Add ingredient
+            </Button>
+          </Form.Control>
+        </Form.Field>
+      </form>
       <div className="is-justify-content-center tags">
         {
-          ingredients.map(i => {
+          ingredientsState.map(i => {
             return <Tag key={i} color="success" rounded>
               {i}
               <Button
